aa9374cb960c229cb87e196be83cfe981d49d32d
Marcelo Bonivento <boniventomarcelo@gmail.com>
2022-04-01 22:17:15 -0500

    Commit inicial del repositorio, codigo sin errores de sintaxis, no se ha podido verificar si es correcta la toma de las muestras

---
+++ main/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#define DEFAULT_VREF    1100
+#define NO_OF_SAMPLES   64      
+
+static const adc_channel_t channel = ADC1_CHANNEL_5;
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten = ADC_ATTEN_11db;
+static const adc_unit_t unit = ADC_UNIT_1;
+
+void app_main(void)
+{
+
+    adc1_config_channel_atten(channel, atten);
+    adc1_config_width(width);
+    esp_adc_cal_characteristics_t *adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+
+    while (1) {
+        uint32_t adc_reading = 0;
+        //Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            adc_reading += adc1_get_raw((adc1_channel_t)channel);
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
